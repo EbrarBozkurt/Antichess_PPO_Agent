360d8c2975bd81847f80f9057858f7780631ed3f
EbrarBozkurt <63541848+EbrarBozkurt@users.noreply.github.com>
2023-12-17 20:48:54 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,1 @@
-import Agent
-from Node import Node
-            
-# Monte Carlo
-agentWhite = Agent.Agent("b")
-print("------------------")
 
-total = 0
-num = 0
-for x in range(4):
-    total += agentWhite.playHuman(100)
-    num += 1
-    print(str(total) + " / " + str(num))
